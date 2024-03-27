fb34848d8869b433d86fc2cae5dd6bfd58de5511
AditiGi <aditisingh20144@gmail.com>
2024-03-27 19:18:11 +0530

    dlklk

---
+++ blueDog/script.js
@@ -0,0 +1,8 @@
+const mainContent = document.querySelector("#homepage-main-content");
+const caption1 = document.querySelector("#eat");
+const caption2 = document.querySelector("#drink");
+const caption3 = document.querySelector("#visit");
+
+// mainContent.addEventListener("hover", function() {
+//   if
+// })
