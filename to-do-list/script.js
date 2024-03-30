5b75070def93a46142016e9eb989ee368681a30b
AditiGi <aditisingh20144@gmail.com>
2024-03-30 14:45:34 +0530

    asdcn

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,17 +1,33 @@
-const taskPlaceholder = document.getElementsByClassName("task-list");
-const taskButton = document.getElementsByClassName("add-button");
-const storedTasks = document.getElementById("taskList");
+// const taskPlaceholder = document.getElementsByClassName("task-list");
+// const taskButton = document.getElementsByClassName("add-button");
+// const storedTasks = document.getElementById("taskList");
 
-function mainTask() {
-  const retrievedText = inputField.value;
+// function mainTask() {
+//   const retrievedText = inputField.value;
 
-  if (retrievedText !== "") {
+//   if (retrievedText.trim !== "") {
+//     const newTask = document.createElement("li");
+
+//     newTask.textContent = retrievedText;
+
+//     storedTasks.appendChild(newTask);
+//   }
+// }
+
+// taskButton.addEventListener("click", mainTask);
+
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+  const taskText = taskInput.value;
+
+  if (taskText !== "") {
     const newTask = document.createElement("li");
-
-    newTask.textContent = retrievedText;
-
-    storedTasks.appendChild(newTask);
+    newTask.textContent = taskText;
+    taskList.appendChild(newTask);
   }
 }
-
-taskButton.addEventListener("click", mainTask);
