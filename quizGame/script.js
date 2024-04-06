721bae18fb1e47cbc9e9814ac7eb1f5819aea059
AditiGi <aditisingh20144@gmail.com>
2024-04-06 21:33:02 +0530

    dsmdz

---
+++ quizGame/script.js
@@ -0,0 +1,69 @@
+const quizButton = document.getElementsByClassName(".quizButton");
+
+const quizQuestions = [
+  {
+    question: "Which planet is known as the 'Red Planet'?",
+    options: ["Venus", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars",
+  },
+  {
+    question: "Who is the author of 'To Kill a Mockingbird'?",
+    options: [
+      "Harper Lee",
+      "J.K. Rowling",
+      "Ernest Hemingway",
+      "F. Scott Fitzgerald",
+    ],
+    answer: "Harper Lee",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Au", "Ag", "Fe", "Cu"],
+    answer: "Au",
+  },
+  {
+    question: "What is the capital of France?",
+    options: ["Rome", "Madrid", "Paris", "Berlin"],
+    answer: "Paris",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    options: [
+      "Vincent van Gogh",
+      "Leonardo da Vinci",
+      "Pablo Picasso",
+      "Michelangelo",
+    ],
+    answer: "Leonardo da Vinci",
+  },
+  {
+    question: "What is the largest mammal in the world?",
+    options: ["African Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+    answer: "Blue Whale",
+  },
+  {
+    question: "Which of the following is not a primary color?",
+    options: ["Red", "Green", "Yellow", "Black"],
+    answer: "Black",
+  },
+  {
+    question: "Who developed the theory of relativity?",
+    options: [
+      "Isaac Newton",
+      "Albert Einstein",
+      "Stephen Hawking",
+      "Galileo Galilei",
+    ],
+    answer: "Albert Einstein",
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    options: ["H2O", "CO2", "NaCl", "O2"],
+    answer: "H2O",
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    options: ["1912", "1908", "1915", "1920"],
+    answer: "1912",
+  },
+];
